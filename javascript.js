ef530ff646afeb9469b5ee61e2ec3ca9fafcbad8
Alexis Gonzalez <a.rgonzalez0951@gmail.com>
2023-10-30 15:51:59 -0700

    Updated Website2.0

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
-function hello_world(){
+function hello_world() {
     alert("Hello World!");
-}
+  }
+  
+document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault(); 
+
+    document.getElementById("contact-form").style.display = "none";
+    document.getElementById("success-message").style.display = "block";
+});
